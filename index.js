cfe73bf9f3f80ebf85e60f9abea11798b8896056
Paulo H B Rodrigues <paulohenriquebispo179@gmail.com>
2024-03-05 11:54:50 -0300

    aula 11

---
+++ index.js
@@ -0,0 +1,14 @@
+// let nome ="Fernanda"
+// let idade = 25;
+// let altura = 1.65;
+// let temGato = false;
+
+// // alert("Olá eu sou "+nome+" tudo bem?")
+
+
+let base = 20
+let altura =50
+
+let resultado = (base* altura)/ 2
+
+alert ("A área total é " + resultado)
